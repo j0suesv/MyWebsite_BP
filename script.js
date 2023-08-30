a858f510d635f82d3b8dc414d05627f4014dde1d
j0suesv <salcedojosue095@gmail.com>
2023-08-30 13:41:26 -0500

    Links are fuctionals now

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const ham = document.querySelector('.ham-icon');
 const links = document.querySelector('.menu-links');
-const bars = document.querySelectorAll('.ham-icon span')
+const bars = document.querySelectorAll('.ham-icon span');
+const aboutLink = document.querySelector(".link1");
+const favSportLink = document.querySelector(".link2");
+const favTeamsLink = document.querySelector(".link3");
+const favPlayersLinks = document.querySelector(".link4");
+
 
 ham.addEventListener('click', () => {
     links.classList.toggle("active");
     bars.forEach(child => {child.classList.toggle("animated")});
-})
+});
+
+aboutLink.addEventListener('click', () => {
+    links.classList.toggle("active");
+    bars.forEach(child => {child.classList.toggle("animated")});
+});
+favSportLink.addEventListener('click', () => {
+    links.classList.toggle("active");
+    bars.forEach(child => {child.classList.toggle("animated")});
+});
+favTeamsLink.addEventListener('click', () => {
+    links.classList.toggle("active");
+    bars.forEach(child => {child.classList.toggle("animated")});
+});
+favPlayersLinks.addEventListener('click', () => {
+    links.classList.toggle("active");
+    bars.forEach(child => {child.classList.toggle("animated")});
+});
 
